9f712551600831ceb0223288ae4f2ea97730282b
Gonzalo Solaligue <106628551+tecomeister@users.noreply.github.com>
2022-10-28 21:39:11 -0300

    Added Player Controller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,94 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] Transform player;
+    [SerializeField] Rigidbody rb;
+    [SerializeField] GameObject camCombat;
+    [SerializeField] GameObject camExploration;
+    [SerializeField] Transform cam;
+    [SerializeField] float rotationSpeed = 5f;
+    [SerializeField] float rotationSmoothTime;
+    [SerializeField] float movementSpeed;
+    [SerializeField] float walkSpeed = 2f;
+    [SerializeField] float runingSpeed = 4f;
+    [SerializeField] CameraType cameraType;
+    float rotationSmoothSpeed;
+    float horizontalInput;
+    float verticalInput;
+
+    public enum CameraType
     {
-        
+        Exploration,
+        Combat
     }
 
-    // Update is called once per frame
-    void Update()
+    void Start()
     {
-        
+        movementSpeed = walkSpeed;
+    }
+
+    void FixedUpdate()
+    {
+        PlayerMovement();
+    }
+
+    void PlayerMovement()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            movementSpeed = runingSpeed;
+        }
+        else
+        {
+            movementSpeed = walkSpeed;
+        }
+
+        if (Input.GetButton("Fire2"))
+        {
+            SetCameraType(CameraType.Combat);
+        }
+        else
+        {
+            SetCameraType(CameraType.Exploration);
+        }
+    }
+
+    void SetCameraType(CameraType cameraType)
+    {
+        switch (cameraType)
+        {
+            case CameraType.Exploration:
+                
+                camExploration.SetActive(true);
+                camCombat.SetActive(false);
+                
+                Vector3 lookdirection = new Vector3(horizontalInput, 0f, verticalInput).normalized;
+                if (lookdirection.magnitude >= 0.1f)
+                {
+                    float targetAngle = Mathf.Atan2(lookdirection.x, lookdirection.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+                    float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref rotationSmoothSpeed, rotationSmoothTime);
+                    transform.rotation = Quaternion.Euler(0f, angle, 0f);
+
+                    Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+
+                    rb.AddForce(moveDir.normalized * movementSpeed * 5f);
+                }
+                break;
+
+            case CameraType.Combat:
+                
+                camExploration.SetActive(false);
+                camCombat.SetActive(true);
+
+                Vector3 direction = transform.forward * verticalInput + transform.right * horizontalInput;
+                rb.AddForce(direction.normalized * movementSpeed * 5f);
+
+                Quaternion lookRotation = Quaternion.Euler(0f, cam.eulerAngles.y, 0f);
+                transform.rotation = Quaternion.Lerp(transform.rotation, lookRotation, Time.deltaTime * rotationSpeed);
+                break;
+        }
     }
 }
